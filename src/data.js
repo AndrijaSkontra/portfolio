7f218d127d038ab4399f4b3baab229ffb4424c4b
Andrija Skontra <askontra22@student.unizd.hr>
2024-09-28 13:19:42 +0200

    decoupled techstack list, and improved on it

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const techStack = [
+  {
+    filePath: "python2.png",
+    tool: `My first real programming experience,
+    started to learn about variables, conditionals, loops and functions.`,
+    date: "First year of coding",
+  },
+  {
+    filePath: "pygame1.png",
+    tool: `Since I was starting to enjoy coding more and more, I made a few simple PyGame video games.`,
+    date: "First year of coding",
+  },
+  {
+    filePath: "java1.png",
+    tool: `Java was my introduction to OOP, and when I started to make Swing application I felt like OOP was the best thing ever (these days I'm starting to like functional paradigm a bit more).`,
+    date: "First year of coding",
+  },
+  {
+    filePath: "datastructure1.png",
+    tool: "Started to learn about various algorithms and data structures, realized linked lists are DOPE.",
+    date: "First year of coding",
+  },
+  {
+    filePath: "linux1.png",
+    tool: "Installed my first Linux Distro (Ubuntu).",
+    date: "Summer from first year going into the second college year.",
+  },
+  {
+    filePath: "archlinux.svg",
+    tool: "Went through my distro hopping phase (Ubuntu -> Endeavour OS -> Fedora -> Arch -> WSL Ubuntu)",
+    date: "Second year of college",
+  },
+  {
+    filePath: "patterns2.png",
+    tool: "Got introduced to design patterns (JUST USE STRATEGY PATTERN EVERYWHERE).",
+    date: "Second year of college",
+  },
+  {
+    filePath: "network1.png",
+    tool: "Had an amazing professor that really made me like computer networking and protocols",
+  },
+  {
+    filePath: "database2.png",
+    tool: "Learned SQL with Sqlite and MySQL.",
+    date: "Second year of college",
+  },
+  {
+    filePath: "nestjs.svg",
+    tool: "Started to learn about basic backend stuff in NESTJS",
+  },
+  {
+    filePath: "angular.svg",
+    tool: "Implemented the backend api from NESTJS to make a simple education system app where professors can grade/post notifications and students can consume that data.",
+    date: "Second year of college",
+  },
+  {
+    filePath: "react.svg",
+    tool: "Starting to really go deeper in frontend knowledge and learning all about React hooks (using nextjs).",
+    date: "Summer going into the third college year",
+  },
+  {
+    filePath: "javascript1.png",
+    tool: "Today I'm building this portfolio to master pure JavaScript, HTML, CSS. Hoping that understanding these tools better will make me better frontend enginner.",
+    date: "Current",
+  },
+];
--- src/main.js
+++ src/main.js
@@ -1,33 +1,18 @@
+import { techStack } from "./data.js";
+
+// get all need document elements
 const shownIcon = document.getElementById("shown-icon");
 const shownDesc = document.getElementById("change-desc");
-
-const tools = [
-  {},
-  { filePath: "pygame1.png", tool: "Pygame" },
-  { filePath: "java1.png", tool: "Java" },
-  { filePath: "datastructure1.png", tool: "Algorithms and Data Structures" },
-  { filePath: "linux1.png", tool: "GNU/Linux" },
-  { filePath: "archlinux.svg", tool: "Distro Hopping" },
-  { filePath: "patterns2.png", tool: "Design Patterns" },
-  {
-    filePath: "network1.png",
-    tool: "Computer Networking and Protocols (Basics)",
-  },
-  { filePath: "database2.png", tool: "Database" },
-  { filePath: "nestjs.svg", tool: "Backend basic app in NESTJS" },
-  { filePath: "angular.svg", tool: "Angular" },
-  { filePath: "react.svg", tool: "React" },
-  { filePath: "javascript1.png", tool: "Pure JavaScript, HTML, CSS" },
-];
-
 const changeImageButton = document.getElementById("change-image");
+
 changeImageButton.addEventListener("click", () => {
   shownIcon.setAttribute(
     "data-number",
     String(Number(shownIcon.getAttribute("data-number")) + 1),
   );
   const currentPicture =
-    "./assets/images/" + tools[shownIcon.getAttribute("data-number")].filePath;
+    "./assets/images/" +
+    techStack[shownIcon.getAttribute("data-number")].filePath;
   shownIcon.setAttribute("src", currentPicture);
-  shownDesc.innerText = tools[shownIcon.getAttribute("data-number")].tool;
+  shownDesc.innerText = techStack[shownIcon.getAttribute("data-number")].tool;
 });
