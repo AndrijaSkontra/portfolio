f6f3ef8145d0f86107fe640c1f3084a023468697
Andrija Skontra <askontra22@student.unizd.hr>
2024-10-03 22:09:13 +0200

    feat: a cool effect when draggable is hovered on the droppable element

--- src/dragndrop.js
+++ src/dragndrop.js
@@ -1,9 +1,32 @@
+// state
 const dragBoxes = Array.from(document.getElementsByClassName("drag-elem"));
 const img = new Image();
 img.src = "../assets/images/droptemp.png";
 
 for (const dragBox of dragBoxes) {
   dragBox.addEventListener("dragstart", dragstartHandler);
+}
+
+//listeners
+const dropBoxes = Array.from(document.getElementsByClassName("droppable-zone"));
+dropBoxes.forEach((elem) => {
+  elem.addEventListener("drop", dropHandler);
+  elem.addEventListener("dragover", dragoverHandler);
+  elem.addEventListener("dragenter", handleEnterEvent);
+  elem.addEventListener("dragleave", (ev) => {
+    makeBackgroundOfElemTransparent(ev.currentTarget);
+  });
+  elem.addEventListener("dragend", (ev) => {
+    makeBackgroundOfElemTransparent(ev.currentTarget);
+  });
+});
+
+// functions
+function handleEnterEvent(ev) {
+  if (checkDoesNodeHaveChildren(ev.target)) {
+    return;
+  }
+  ev.currentTarget.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
 }
 
 function dragstartHandler(currentEvent) {
@@ -14,11 +37,13 @@ function dragstartHandler(currentEvent) {
   );
 }
 
-const dropBoxes = Array.from(document.getElementsByClassName("droppable-zone"));
-dropBoxes.forEach((elem) => {
-  elem.addEventListener("drop", dropHandler);
-  elem.addEventListener("dragover", dragoverHandler);
-});
+function checkDoesNodeHaveChildren(node) {
+  return !dropBoxes.includes(node) || node.childElementCount > 0;
+}
+
+function makeBackgroundOfElemTransparent(elem) {
+  elem.style.backgroundColor = "transparent";
+}
 
 function dragoverHandler(ev) {
   ev.preventDefault();
@@ -26,10 +51,11 @@ function dragoverHandler(ev) {
 }
 function dropHandler(ev) {
   ev.preventDefault();
-  if (!dropBoxes.includes(ev.target) || ev.target.childElementCount > 0) {
+  if (checkDoesNodeHaveChildren(ev.target)) {
     return;
   }
   const data = ev.dataTransfer.getData("application/my-node");
   const draggedElement = document.getElementById(data);
   ev.target.appendChild(draggedElement);
+  makeBackgroundOfElemTransparent(ev.target);
 }
